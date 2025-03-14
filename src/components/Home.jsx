8fc9b7afba94c4de6c5b9be7421f9516f4525ca0
Vivek Yadav <vyadav166@gmail.com>
2025-03-14 17:04:29 +0530

    New Change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,14 +39,7 @@ const Home = () => {
             mollitia. Dolor placeat repellendus officia aspernatur dolorum
             harum. Dolorem cumque quaerat eius qui deserunt ullam laudantium
             quia tempore. Dignissimos esse sequi numquam est ipsam nemo,
-            voluptas vel recusandae iste aspernatur qui quasi, commodi, atque
-            ipsa dolores tempore asperiores? Dolores soluta placeat molestiae
-            aperiam, ullam officia fugiat corrupti odio, suscipit ipsum cum vel
-            nemo. Ratione quo tempore fugit quaerat! Facilis beatae impedit
-            similique maxime praesentium sunt unde necessitatibus voluptates
-            ipsa, et, numquam reiciendis eveniet? Aliquid nemo nesciunt placeat,
-            qui fuga quae repellat sunt, non natus, iure ab officia rem ipsa cum
-            sed cumque corporis voluptate quibusdam nostrum quod saepe
+            voluptas vel recusandae iste aspernatur qui quasi, commodi.
           </p>
         </div>
       </div>
